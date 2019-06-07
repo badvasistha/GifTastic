dcf15750e4059e3ae75d7ef3464a91ddda7cacc8
Badri Vasistha <badri.vasistha@gmail.com>
2019-06-07 10:01:11 -0700

    API key works, gifs play and pause.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,88 @@
-console.log(this)
+$(document).ready(function() {
+    //Array for searched topics to be added
+    var topics = [];
+    
+        //Function with AJAX call to GIPHY; Q parameterc for API link set to search term, limit 10 results
+      //Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
+         function renderGifs() {
+    
+        var x = $(this).data("search");
+        console.log(x);
+    
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=bizSXWapvvd81SeBYlL80YTAMYP8t7TU";
+    
+        console.log(queryURL);
+    
+        $.ajax({
+              url: queryURL,
+              method: "GET"
+            }).done(function(response) {
+                var results = response.data;
+                console.log(results);
+                for (var i = 0; i < results.length; i++) {
+                
+                var showDiv = $("<div class='col-md-4'>");
+    
+                var rating = results[i].rating;
+                var defaultAnimatedSrc = results[i].images.fixed_height.url;
+                var staticSrc = results[i].images.fixed_height_still.url;
+                var showImage = $("<img>");
+                var p = $("<p>").text("Rating: " + rating);
+    
+                showImage.attr("src", staticSrc);
+                showImage.addClass("Giphy");
+                showImage.attr("data-state", "still");
+                showImage.attr("data-still", staticSrc);
+                showImage.attr("data-animate", defaultAnimatedSrc);
+                showDiv.append(p);
+                showDiv.append(showImage);
+                $("#gifArea").prepend(showDiv);
+    
+            }
+        });
+    }
+    
+      //Submit button click event takes search term from form input, trims and pushes to topics array, displays button
+        $("#searchGIF").on("click", function(event) {
+            event.preventDefault();
+            var newGIF = $("#gifInput").val().trim();
+            topics.push(newGIF);
+            console.log(topics);
+            $("#gifInput").val('');
+            displayButtons();
+          });
+    
+      //Function iterates through topics array to display button with array values in "myButtons" section of HTML
+        function displayButtons() {
+        $("#myButtons").empty();
+        for (var i = 0; i < topics.length; i++) {
+          var a = $('<button class="btn btn-primary">');
+          a.attr("id", "show");
+          a.attr("data-search", topics[i]);
+          a.text(topics[i]);
+          $("#myButtons").append(a);
+        }
+      }
+    
+    
+      displayButtons();
+    
+      //Click event on button with id of "show" executes renderGifs function
+      $(document).on("click", "#show", renderGifs);
+    
+      //Click event on gifs with class of "Giphy" executes pausePlayGifs function
+      $(document).on("click", ".Giphy", pausePlayGifs);
+    
+      //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
+      function pausePlayGifs() {
+           var state = $(this).attr("data-state");
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+      }
+    }
+    
+    });
